2a1e6310bd6d96916bed6a7806b42bb16a48a41f
valerieruiz <valerieruiz212024@gmail.com>
2023-01-10 12:52:33 -0500

    background

---
+++ sketch copy.js
@@ -0,0 +1,33 @@
+//create a program that can customize the number of squares in a grid
+
+let gif;
+
+function preload(){ //preload image or gif
+  gif = loadImage('assets/ai_background.gif');
+}
+
+function setup() { // runs once
+  createCanvas(windowWidth, windowWidth); //canvas is 720x720 pixels
+  background (252, 250, 252); //rgb
+
+}
+
+function draw() { // runs in a loop
+  fill (252, 250, 252); //fills the next shape
+  strokeWeight(1); //adjust stroke weight of the square
+
+  var num = 20; //number of squares in the array
+  var sideLength = windowWidth/num;
+
+translate (-150, -150); //gives bleeding effect
+
+  for (var y = 0; y < 2 * windowWidth; y = y+sideLength){
+    for (var x = 0; x < 2 * windowWidth; x = x+sideLength){ // loop creates a row in the x direction (top two squares)
+      image (gif, x, y, windowWidth/num, windowWidth/num);
+    }
+  }
+}
+
+function windowResized(){ //resizes and refreshes design with the browser
+  resizeCanvas(windowWidth, windowHeight);
+}
